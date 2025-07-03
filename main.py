8640e7b9ffe65f80609cd0d174b9c796847e71b2
ChinweA <chinweamakom@yahoo.com>
2025-07-03 21:42:57 +0100

    new commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from transport.node import Node, RingBuffer
+def message(_in_buffer, _out_buffer):
+    print("Hey")
+
+# node = Node(id="12", in_size = 10, out_size= 10, action=message)
+
+# node.run_action()
+
---
+++ transport/node.py
@@ -0,0 +1,61 @@
+
+from typing import Any, Callable, List, Optional
+from pydantic import BaseModel, Field, PrivateAttr
+
+
+class RingBuffer(BaseModel):
+    size: int
+    buffer: List[Optional[Any]] = Field(default_factory=list)
+
+    _head: int = PrivateAttr(0)
+    _tail: int = PrivateAttr(0)
+    _full: bool = PrivateAttr(False)
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.buffer = [None] * self.size
+
+    def append(self, item: Any):
+        self.buffer[self._head] = item
+        if self._full:
+            self._tail = (self._tail + 1) % self.size
+        self._head = (self._head + 1) % self.size
+        self._full = self._head == self._tail
+
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("Buffer is empty")
+        item = self.buffer[self._tail]
+        self.buffer[self._tail] = None
+        self._tail = (self._tail + 1) % self.size
+        self._full = False
+        return item
+
+    def is_empty(self):
+        return not self._full and self._head == self._tail
+
+    def is_full(self):
+        return self._full
+
+
+class Node(BaseModel):
+    id: str
+    in_size: int
+    out_size: int
+    action: Callable[[RingBuffer, RingBuffer], None]
+
+    _in_buffer: RingBuffer = PrivateAttr()
+    _out_buffer: RingBuffer = PrivateAttr()
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self._in_buffer = RingBuffer(size=self.in_size)
+        self._out_buffer = RingBuffer(size=self.out_size)
+
+    def run_action(self):
+        return self.action(self._in_buffer, self._out_buffer)
+    
+
+
+
+
